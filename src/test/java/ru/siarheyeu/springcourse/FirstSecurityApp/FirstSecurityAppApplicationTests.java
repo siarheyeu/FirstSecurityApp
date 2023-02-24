4acf0305327888043ddc6dead422b7166657ad99
Siarheyeu <115313319+siarheyeu@users.noreply.github.com>
2023-02-25 00:03:00 +0300

    Initial commit

---
+++ src/test/java/ru/siarheyeu/springcourse/FirstSecurityApp/FirstSecurityAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.siarheyeu.springcourse.FirstSecurityApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSecurityAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
