d93a159c85f28fc05f788f3ee74dcfb3d799a938
Siarheyeu <115313319+siarheyeu@users.noreply.github.com>
2023-02-27 19:21:45 +0300

    HelloController

---
+++ src/main/java/ru/siarheyeu/springcourse/FirstSecurityApp/controllers/HelloController.java
@@ -0,0 +1,16 @@
+package ru.siarheyeu.springcourse.FirstSecurityApp.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+    @GetMapping("/hello")
+    public String sayHello(){
+        return "hello";
+
+    }
+
+
+}
