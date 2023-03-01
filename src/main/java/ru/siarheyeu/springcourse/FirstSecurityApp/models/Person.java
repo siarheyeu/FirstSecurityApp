d6eed88b3f22fdbf4332bd57e888acce29493f5f
Siarheyeu <115313319+siarheyeu@users.noreply.github.com>
2023-03-01 23:59:39 +0300

    Models

---
+++ src/main/java/ru/siarheyeu/springcourse/FirstSecurityApp/models/Person.java
@@ -0,0 +1,10 @@
+package ru.siarheyeu.springcourse.FirstSecurityApp.models;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = Person)
+public class Person {
+}
