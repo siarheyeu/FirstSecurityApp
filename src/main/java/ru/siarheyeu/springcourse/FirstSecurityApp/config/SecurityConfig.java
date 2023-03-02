3f6a97feda79aa8dd8d9ce2508b0fc48dd34f1bd
Siarheyeu <115313319+siarheyeu@users.noreply.github.com>
2023-03-02 23:58:09 +0300

    Security Configuration

---
+++ src/main/java/ru/siarheyeu/springcourse/FirstSecurityApp/config/SecurityConfig.java
@@ -0,0 +1,9 @@
+package ru.siarheyeu.springcourse.FirstSecurityApp.config;
+
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+}
