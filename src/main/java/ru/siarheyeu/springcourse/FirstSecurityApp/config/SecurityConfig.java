d7c9fc9e4acc3cff809da8c98473524768cf0058
Siarheyeu <115313319+siarheyeu@users.noreply.github.com>
2023-03-03 22:48:57 +0300

    AuthProviderImpl

--- src/main/java/ru/siarheyeu/springcourse/FirstSecurityApp/config/SecurityConfig.java
+++ src/main/java/ru/siarheyeu/springcourse/FirstSecurityApp/config/SecurityConfig.java
@@ -1,9 +1,16 @@
 package ru.siarheyeu.springcourse.FirstSecurityApp.config;
 
 
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter{
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    //Настраивает аутентификацию
+protected void configure(AuthenticationManagerBuilder auth){
+    auth.authenticationProvider();
+}
 }
