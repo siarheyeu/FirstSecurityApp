e29a1733747f1a7ef0da9d05062afeff98cd5530
Siarheyeu <115313319+siarheyeu@users.noreply.github.com>
2023-03-05 01:19:30 +0300

    HelloController modification

--- src/main/java/ru/siarheyeu/springcourse/FirstSecurityApp/controllers/HelloController.java
+++ src/main/java/ru/siarheyeu/springcourse/FirstSecurityApp/controllers/HelloController.java
@@ -5,6 +5,9 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import ru.siarheyeu.springcourse.FirstSecurityApp.security.PersonDetails;
+
+import java.sql.SQLOutput;
 
 @Controller
 public class HelloController {
@@ -13,8 +16,12 @@ public class HelloController {
         return "hello";
 
         @GetMapping("/showUserInfo")
-                public String showUserInfo(){
+            public String showUserInfo(){
             Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            PersonDetails personDetails (PersonDetails)authentication.getPrincipal();
+            System.out.println(personDetails.getPerson());
+
+            return "hello";
         }
 
     }
