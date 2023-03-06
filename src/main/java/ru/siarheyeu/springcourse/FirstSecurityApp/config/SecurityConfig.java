7a68aa2b47222d81bf732990c7852b86377b3c47
Siarheyeu <115313319+siarheyeu@users.noreply.github.com>
2023-03-06 19:52:39 +0300

    Сustom authorization page

--- src/main/java/ru/siarheyeu/springcourse/FirstSecurityApp/config/SecurityConfig.java
+++ src/main/java/ru/siarheyeu/springcourse/FirstSecurityApp/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -25,8 +26,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
         this.personDetailsService = personDetailsService;
     }
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        //конфигурируем сам Spring Security
+        //конфигурируем авторизацию
+        http.formLogin().loginPage("/auth/login")
+                .loginProcessingUrl("process_login")
+    }
 
     //Настраивает аутентификацию
+    @Override
 protected void configure(AuthenticationManagerBuilder auth) throws Exception{
     auth.userDetailsService(personDetailsService);
 }
