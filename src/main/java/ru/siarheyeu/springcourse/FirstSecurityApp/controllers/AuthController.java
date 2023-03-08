304434a34e6d3ffa68eff63c7ce83ede2c23957b
Siarheyeu <115313319+siarheyeu@users.noreply.github.com>
2023-03-08 23:59:46 +0300

    Registration process

--- src/main/java/ru/siarheyeu/springcourse/FirstSecurityApp/controllers/AuthController.java
+++ src/main/java/ru/siarheyeu/springcourse/FirstSecurityApp/controllers/AuthController.java
@@ -2,7 +2,9 @@ package ru.siarheyeu.springcourse.FirstSecurityApp.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ru.siarheyeu.springcourse.FirstSecurityApp.models.Person;
 
 @Controller
 @RequestMapping("/auth")
@@ -11,5 +13,9 @@ public class AuthController {
     @GetMapping("/login")
     public String loginPage(){
         return "auth/login";
+    }
+
+    public String registrationPage(@ModelAttribute("person") Person person){
+        return "auth/registration";
     }
 }
