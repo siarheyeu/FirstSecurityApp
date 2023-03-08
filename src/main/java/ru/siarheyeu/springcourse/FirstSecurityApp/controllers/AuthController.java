4ffae90a7de03469918d352d4570217aacc91c26
Siarheyeu <115313319+siarheyeu@users.noreply.github.com>
2023-03-09 00:27:59 +0300

    PersonValidator

--- src/main/java/ru/siarheyeu/springcourse/FirstSecurityApp/controllers/AuthController.java
+++ src/main/java/ru/siarheyeu/springcourse/FirstSecurityApp/controllers/AuthController.java
@@ -1,8 +1,10 @@
 package ru.siarheyeu.springcourse.FirstSecurityApp.controllers;
 
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import ru.siarheyeu.springcourse.FirstSecurityApp.models.Person;
 
@@ -15,7 +17,13 @@ public class AuthController {
         return "auth/login";
     }
 
+    @GetMapping("/registration")
     public String registrationPage(@ModelAttribute("person") Person person){
         return "auth/registration";
+    }
+
+    @PostMapping("/registration")
+    public String performRegistration(@ModelAttribute("person") @Valid Person person){
+
     }
 }
---
+++ src/main/java/ru/siarheyeu/springcourse/FirstSecurityApp/util/PersonValidator.java
@@ -0,0 +1,35 @@
+package ru.siarheyeu.springcourse.FirstSecurityApp.util;
+
+import jakarta.validation.Validator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import ru.siarheyeu.springcourse.FirstSecurityApp.models.Person;
+import ru.siarheyeu.springcourse.FirstSecurityApp.services.PersonDetailsService;
+
+@Component
+public class PersonValidator implements Validator {
+
+    private final PersonDetailsService personDetailsService;
+
+    @Autowired
+    public PersonValidator(PersonDetailsService personDetailsService) {
+        this.personDetailsService = personDetailsService;
+    }
+
+    @Override
+    public boolean supports (Class<?> aClass){
+        return Person.class.equals(aClass);
+    }
+
+    @Override
+    public void validate (Object o, Errors errors) {
+        Person person = (Person)o;
+    }
+        try
+
+             {
+        personDetailsService.loadUserByUsername(person.getUsername());
+             }
+    }
+}
