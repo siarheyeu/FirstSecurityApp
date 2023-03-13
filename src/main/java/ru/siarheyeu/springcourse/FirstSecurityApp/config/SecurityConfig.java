00be7dbf8e30f6d3cbc3d800e582f47ef9eaf8bc
Siarheyeu <115313319+siarheyeu@users.noreply.github.com>
2023-03-13 21:00:38 +0300

    @PreAuthorize

--- src/main/java/ru/siarheyeu/springcourse/FirstSecurityApp/config/SecurityConfig.java
+++ src/main/java/ru/siarheyeu/springcourse/FirstSecurityApp/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -14,7 +15,7 @@ import ru.siarheyeu.springcourse.FirstSecurityApp.services.PersonDetailsService;
 
 
 @EnableWebSecurity
-@Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
     private final AuthProviderImpl authProvider;
@@ -32,7 +33,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
         //конфигурируем сам Spring Security
         //конфигурируем авторизацию
                 http.authorizeHttpRequests()
-                        .antMatchers("/admin").hasRole("ADMIN")
                 .antMatchers("/auth/login", "/auth/registration", "/error").permitAll()
                 .anyRequest().hasAnyRole("USER", "ADMIN")
                 .and()
