0651986fce021235b53a44f2b58f72aa83488b90
Siarheyeu <115313319+siarheyeu@users.noreply.github.com>
2023-04-26 18:36:58 +0300

    AuthenticationDTO

--- src/main/java/ru/siarheyeu/springcourse/FirstSecurityApp/dto/AuthenticationDTO.java
+++ src/main/java/ru/siarheyeu/springcourse/FirstSecurityApp/dto/AuthenticationDTO.java
@@ -1,17 +1,12 @@
 package ru.siarheyeu.springcourse.FirstSecurityApp.dto;
 
-import jakarta.persistence.Column;
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
 
-public class PersonDTO {
+public class AuthenticationDTO {
     @NotEmpty(message = "Имя не должно быть пустым")
     @Size(min = 2, max = 100, message = "Имя должно быть от 2 до 100 символов длиной")
     private String username;
-
-    @Min(value = 1900, message = "Год рождения должен быть больше, чем 1900")
-    private int yearOfBirth;
 
     private String password;
 
@@ -21,14 +16,6 @@ public class PersonDTO {
 
     public void setUsername(String username) {
         this.username = username;
-    }
-
-    public int getYearOfBirth() {
-        return yearOfBirth;
-    }
-
-    public void setYearOfBirth(int yearOfBirth) {
-        this.yearOfBirth = yearOfBirth;
     }
 
     public String getPassword() {
